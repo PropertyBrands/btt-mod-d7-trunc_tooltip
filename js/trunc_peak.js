d52976af0154ace2520405aa11a7c1e078aa0603
tanmancan <contact@tkarimdesign.com>
2016-06-20 14:41:15 -0400

    install file, added conditional and once wrapper

--- js/trunc_peak.js
+++ js/trunc_peak.js
@@ -1,8 +1,15 @@
 (function ($, Drupal) {
 	Drupal.behaviors.trunc_peak = {
 		attach: function(context, settings) {
-			var selectors = settings;
-			console.log(selectors);
+			if(Drupal.settings.trunc_peak) {
+				var $selectors = $(settings.trunc_peak.selectors);
+				$('body', context).once('trunc-peak-processed', function() {
+					console.log($selectors);
+					$.each($selectors, function(idx, el) {
+						
+					});
+				});
+			}
 		}
 	}
 })(jQuery, Drupal);
