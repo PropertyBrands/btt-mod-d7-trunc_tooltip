b546b4348815b3c155d13d6dd73cb70d8a8284c0
tanmancan <contact@tkarimdesign.com>
2016-06-20 15:15:43 -0400

    Added hover functionalty for tooltip. added styles

--- js/trunc_peak.js
+++ js/trunc_peak.js
@@ -2,12 +2,40 @@
 	Drupal.behaviors.trunc_peak = {
 		attach: function(context, settings) {
 			if(Drupal.settings.trunc_peak) {
-				var $selectors = $(settings.trunc_peak.selectors);
 				$('body', context).once('trunc-peak-processed', function() {
-					console.log($selectors);
+					var $selectors = $(settings.trunc_peak.selectors),
+							truncToolTip = document.createElement('span'),
+							bodyTag = document.getElementsByTagName('body')[0];
+
+					truncToolTip.className += 'truncate-tooltip';
+					truncToolTip.style.position = 'absolute';
+					truncToolTip.style.zIndex = 999;
+					truncToolTip.style.display = 'none';
+					bodyTag.appendChild(truncToolTip);
+
 					$.each($selectors, function(idx, el) {
-						
+						var $this = $(el),
+								elText = el.innerHTML;
+
+						el.addEventListener('mouseover', showToolTip, false);
 					});
+
+					function showToolTip(e) {
+						var el = e.target,
+								pageX = e.pageX,
+								pageY = e.pageY,
+								elText = el.innerHTML;
+
+						console.log(e, el.getBoundingClientRect());
+
+						if(el.scrollWidth > el.clientWidth) {
+							truncToolTip.innerHTML = elText;
+							truncToolTip.style.display = 'inline-block';
+							truncToolTip.style.top = pageY + 'px';
+							truncToolTip.style.left = pageX + 'px';
+						}
+
+					}
 				});
 			}
 		}
