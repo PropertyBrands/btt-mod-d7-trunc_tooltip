506601db06e29fdc0b1d95c74c8b22b3092d2428
tanmancan <contact@tkarimdesign.com>
2016-06-23 09:47:14 -0400

    Rewrote script to take into account Drupal attach behaviors - wrap .once only around event listener function

--- js/trunc_tooltip.js
+++ js/trunc_tooltip.js
@@ -1,49 +1,76 @@
 (function ($, Drupal) {
 	Drupal.behaviors.trunc_tooltip = {
 		attach: function(context, settings) {
-			
+			var _this = this,
+					$selectors = $(settings.trunc_tooltip.selectors);
+
 			if(Drupal.settings.trunc_tooltip) {
-				$('body', context).once('trunc-tooltip-processed', function() {
-					var $selectors = $(settings.trunc_tooltip.selectors),
-							truncToolTip = document.createElement('span'),
-							bodyTag = document.getElementsByTagName('body')[0];
-
-					truncToolTip.className += 'truncate-tooltip';
-					truncToolTip.style.position = 'absolute';
-					truncToolTip.style.zIndex = 999;
-					truncToolTip.style.display = 'none';
-					bodyTag.appendChild(truncToolTip);
-
-					$.each($selectors, function(idx, el) {
-						var $this = $(el),
-								elText = el.innerHTML;
-
-						el.addEventListener('mouseover', showToolTip, false);
-						el.addEventListener('mouseout', hideToolTip, false);
-					});
-
-					function showToolTip(e) {
-						var el = e.target,
-								pageX = e.pageX + 5,
-								pageY = e.pageY + 5,
-								elText = el.innerHTML;
-
-						if(el.scrollWidth > el.clientWidth) {
-							truncToolTip.innerHTML = elText;
-							truncToolTip.style.display = 'inline-block';
-							truncToolTip.style.top = pageY + 'px';
-							truncToolTip.style.left = pageX + 'px';
-						}
-					}
-
-					function hideToolTip(e) {
-						truncToolTip.innerHTML = '';
-						truncToolTip.style.display = 'none';
-						truncToolTip.style.top = 0 + 'px';
-						truncToolTip.style.left = 0 + 'px';
-					}
+				$selectors.once('truncate-tooltip-processed').each(function(idx, el) {
+					el.addEventListener('mouseover', _this, false);
+					el.addEventListener('mouseout', _this, false);
 				});
 			}
-		}
+		},
+		toolTip: null,
+		buildToolTipSpan: function() {
+			var toolTip = document.createElement('span'),
+					_this = this;
+
+			toolTip.style.position = 'absolute';
+			toolTip.style.zIndex = 999;
+			toolTip.style.display = 'inline-block';
+
+			toolTip.className += 'truncate-tooltip';
+
+			return toolTip;
+		},
+		handleEvent: function(e) {
+			var el = e.target,
+					text = el.innerHTML,
+					bodyTag = document.getElementsByTagName('body')[0],
+					posX = e.pageX + 5
+					posY = e.pageY + 5,
+					_this = this;
+
+			if(!_this.toolTip) {
+				_this.toolTip = _this.buildToolTipSpan();
+			}
+
+			_this.toolTip.style.top = posY + 'px';
+			_this.toolTip.style.left = posX + 'px';
+			_this.toolTip.innerHTML = text;
+
+			console.log(this);
+			console.log(e);
+			console.log(_this.toolTip);
+			switch(e.type) {
+				case 'mouseover':
+					bodyTag.appendChild(_this.toolTip);
+					break;
+				case 'mouseout':
+					_this.toolTip.remove();
+					break
+			}
+		},
+		showToolTip: function(e) {
+			var el = e.target,
+					pos = {},
+					text = el.innerHTML,
+					_this = this;
+
+			pos.pageX = e.pageX + 5;
+			pos.pageY = e.pageY + 5;
+
+			if(el.scrollWidth > el.clientWidth) {
+				console.log(_this);
+			}
+			return this;
+		},
+		hideToolTip: function(e) {
+
+			console.log(this, _this);
+			return this;
+
+		},
 	};
 })(jQuery, Drupal);
